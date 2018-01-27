83b6cb6ce80b285cf3d312d5983d270af43d742a
Matroskeen <Matroskeen@3426249.no-reply.drupal.org>
2018-01-27 18:45:22 +0200

    Issue #2939579 by Matroskeen: Added tests for settings form.

--- tests/src/Functional/MailgunFunctionalTestBase.php
+++ tests/src/Functional/MailgunFunctionalTestBase.php
@@ -2,12 +2,15 @@
 
 namespace Drupal\Tests\mailgun\Functional;
 
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\Tests\BrowserTestBase;
 
 /**
  * Base test class for Mailgun functional tests.
  */
 abstract class MailgunFunctionalTestBase extends BrowserTestBase {
+
+  use StringTranslationTrait;
 
   /**
    * Modules to enable.
@@ -24,5 +27,20 @@ abstract class MailgunFunctionalTestBase extends BrowserTestBase {
   protected $permissions = [
     'administer mailgun',
   ];
+
+  /**
+   * An editable config.
+   *
+   * @var \Drupal\Core\Config\Config
+   */
+  protected $mailgunConfig;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    $this->mailgunConfig = $this->config(MAILGUN_CONFIG_NAME);
+  }
 
 }
