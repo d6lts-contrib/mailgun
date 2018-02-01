dbe94854716eca37403e5e36c8053b3c585bdd7e
markdorison <markdorison@346106.no-reply.drupal.org>
2018-02-01 23:01:31 +0200

    Issue #2941454 by markdorison: PHPCS Warning: Possible useless method overriding detected

--- src/Form/MailgunTestEmailForm.php
+++ src/Form/MailgunTestEmailForm.php
@@ -171,13 +171,6 @@ If this e-mail is displayed correctly and delivered sound and safe, congrats! Yo
   /**
    * {@inheritdoc}
    */
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-    parent::validateForm($form, $form_state);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $to = $form_state->getValue('to');
 
